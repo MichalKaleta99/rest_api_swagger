d11054416d69df6dfbf1fac767e702adee58593a
mikaleta <thekalkam@gmail.com>
2023-01-31 14:22:37 +0100

    second commit

---
+++ contacts/src/main/java/com/ltp/contacts/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.ltp.contacts.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info().title("CONTACT API")
+                .description("An API that manages contact requests")
+                .version("1.0.1")
+                )
+                ;
+    }
+
+}
